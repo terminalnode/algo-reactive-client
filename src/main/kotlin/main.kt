cbfe1efe96dc6080f6ac6628836f66e5356e948f
Alexander Rundberg <alexanderrundberg@fastmail.jp>
2021-05-16 15:20:44 +0200

    make button weirdly display latest block summary

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,7 +4,6 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.unit.IntSize
 import kotlinx.coroutines.CoroutineScope
@@ -14,28 +13,33 @@ import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToFlux
 import se.newton.algocompose.algoapi.models.ShortBlockSummary
 
-fun main() = Window(
-	title = "Newton AlgoCompose",
-	size = IntSize(300, 300)
-) {
-	var text by remember { mutableStateOf("Hello, World!") }
+fun main() {
+	return Window(
+		title = "Newton AlgoCompose",
+		size = IntSize(300, 300)
+	) {
+		//var text by remember { mutableStateOf("Hello, World!") }
+		var text by mutableStateOf("Initial value")
 
-	CoroutineScope(IO).launch {
-		val webClient = WebClient.create("http://localhost:8080")
+		CoroutineScope(IO).launch {
+			val webClient = WebClient.create("http://localhost:8080")
 
-		val response = webClient.get()
-			.uri("/algo/block/summary-flux")
-			.retrieve()
-			.bodyToFlux<ShortBlockSummary>()
-			.map {
-				return@map it
+			val response = webClient.get()
+				.uri("/algo/block/summary-flux")
+				.retrieve()
+				.bodyToFlux<ShortBlockSummary>()
+				.map {
+					return@map it
+				}
+			response.subscribe {
+				text = it.toString()
 			}
-		response.subscribe { println(it) }
-	}
+		}
 
-	MaterialTheme {
-		Button(onClick = { text = "Hello, Desktop!" }) {
-			Text(text)
+		MaterialTheme {
+			Button(onClick = { text = "Hello, Desktop!" }) {
+				Text(text)
+			}
 		}
 	}
 }
